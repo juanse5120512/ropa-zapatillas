c85c50e1e12e984bd46f857c84f79ef06fe000e2
Juan Sebastian Badone <juansb2001@hotmail.com>
2022-12-02 16:16:54 -0300

    arreglando controller

--- app/controllers/zap_controller.php
+++ app/controllers/zap_controller.php
@@ -2,8 +2,6 @@
 require_once './app/models/zap_model.php';
 require_once './app/views/zap_view.php';
 
-
-
 class zapController {
     private $modelZap;
     private $view;
@@ -16,8 +14,14 @@ class zapController {
 
     public function showShoes_c(){
         session_start();
-        $zapatillas = $this->modelZap->getShoes_m();
+        $zapatillas = $this->modelZap->getAll_shoes_m();
         $this->view->show_shoes_v($zapatillas);
     }
+
+    public function showZapatilla_byid($id){
+        $zapatilla = $this->modelZap->getZapatilla_byID_m($id);
+        $this->view->show_shoe_byid_v($zapatilla);
+    }
+
 
 }
